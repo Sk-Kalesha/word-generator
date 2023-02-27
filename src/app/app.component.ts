5220c9294cd46a6a0f447ab7b2ea31201577d5b4
Sk-Kalesha <kaleemuddeen.shaik@gmail.com>
2023-02-27 10:46:15 +0530

    word-generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import arrayWords from '../utils/words';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
+
   title = 'word-generator';
+
+  words = '';
+  limit = 10;
+  randomNumber = (Math.ceil(Math.random()*100));
+
+  handleSlideChange(newLimit: any){    
+    this.limit = newLimit.target.value;
+  }
+
+  generateRandomWords(){
+    this.words = arrayWords.slice(0, this.randomNumber).join(' ');  
+    this.randomNumber = (Math.ceil(Math.random()*100))  
+  }
+
+  generate() {
+    this.words = arrayWords.slice(0, this.limit).join(' ');
+  }
 }
